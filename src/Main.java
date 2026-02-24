7ed5712fe6eb6c83196a7fde1fb51316ded15915
shaiksahid28 <directorsahidbabu@gmail.com>
2026-02-24 14:54:02 +0530

    UC4: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,30 @@
 import java.util.Scanner;
 
-public class UseCase3PalindromeCheckerApp {
+public class UseCase4PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== UC3: User Input Palindrome Checker ===");
+        System.out.println("=== UC4: Two Pointer Palindrome Checker ===");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        String reversed = "";
+        String normalized = input.replaceAll("\\s+", ""); // remove spaces
+        int left = 0;
+        int right = normalized.length() - 1;
+        boolean isPalindrome = true;
 
-        for (int i = input.length() - 1; i >= 0; i--) {
-            reversed += input.charAt(i);
+        while (left < right) {
+            if (normalized.charAt(left) != normalized.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
         }
 
-        if (input.equals(reversed)) {
+        if (isPalindrome) {
             System.out.println("Result: It is a Palindrome.");
         } else {
             System.out.println("Result: It is NOT a Palindrome.");
