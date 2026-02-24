efc31dfdbb967671adab959ceaa16ae2f50bcf0e
SHAIK MAHAMMAD SAHID BABU <directorsahidbabu@gmail.com>
2026-02-24 16:35:27 +0530

    Refactor UseCasePalindromeCheckerApp by removing comments
    Removed comments and adjusted formatting for clarity.

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -2,12 +2,7 @@ import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Scanner;
 
-/**
- * =============================================================
- * MAIN CLASS - UseCase7PalindromeCheckerApp
- * =============================================================
- * * Use Case 7: Deque Based Optimized Palindrome Checker
- */
+
 public class UseCasePalindromeCheckerApp {
 
     public static void main(String[] args) {
@@ -17,23 +12,22 @@ public class UseCasePalindromeCheckerApp {
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        // Normalize string
         String normalized = input.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
 
-        // Create a Deque to store characters
+       
         Deque<Character> deque = new ArrayDeque<>();
 
-        // Add each character to the deque
+     
         for (char c : normalized.toCharArray()) {
             deque.addLast(c);
         }
 
-        // Flag to track palindrome result
+       
         boolean isPalindrome = true;
 
-        // Compare until empty or one character left
+      
         while (deque.size() > 1) {
-            // Remove first & last and compare
+          
             if (deque.removeFirst() != deque.removeLast()) {
                 isPalindrome = false;
                 break;
