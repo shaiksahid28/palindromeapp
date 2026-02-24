4427255b34e408ad62eedc7ebfcd189cd559e33a
SHAIK MAHAMMAD SAHID BABU <directorsahidbabu@gmail.com>
2026-02-24 15:43:30 +0530

    Rename Main class to UseCasePalindromeCheckerApp

--- src/UseCasePalindromeCheckerApp.java
+++ src/UseCasePalindromeCheckerApp.java
@@ -1,4 +1,4 @@
-public class Main {
+public class UseCasePalindromeCheckerApp {
     public static void main(String[] args) {
         // UC1: Welcome message module
         System.out.println("Welcome to the Palindrome Checker App!");
