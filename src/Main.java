5ecd3eb6aad06628aded1a5b03b002924aea419d
shaiksahid28 <directorsahidbabu@gmail.com>
2026-02-24 14:46:37 +0530

    UC3: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,27 @@
-public class Main {
-    public static void main(String[] args) {
-        // UC2: Hardcoded palindrome check
-        String str = "madam"; // hardcoded string
-        String reversed = new StringBuilder(str).reverse().toString();
+import java.util.Scanner;
 
-        if(str.equals(reversed)) {
-            System.out.println(str + " is a palindrome!");
-        } else {
-            System.out.println(str + " is NOT a palindrome!");
+public class UseCase3PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("=== UC3: User Input Palindrome Checker ===");
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+
+        String reversed = "";
+
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reversed += input.charAt(i);
         }
+
+        if (input.equals(reversed)) {
+            System.out.println("Result: It is a Palindrome.");
+        } else {
+            System.out.println("Result: It is NOT a Palindrome.");
+        }
+
+        scanner.close();
     }
 }
