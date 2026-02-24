7a6c1a7dc1309c22a689cf32a359290b156d0eae
shaiksahid28 <directorsahidbabu@gmail.com>
2026-02-24 15:04:27 +0530

    UC5: Add welcome message module

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 
-public class UseCase4PalindromeCheckerApp {
+public class UseCase5PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== UC4: Two Pointer Palindrome Checker ===");
+        System.out.println("=== UC5: Case-Insensitive Palindrome Checker ===");
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        String normalized = input.replaceAll("\\s+", ""); // remove spaces
+        // Normalize: remove spaces and convert to lowercase
+        String normalized = input.replaceAll("\\s+", "").toLowerCase();
+
         int left = 0;
         int right = normalized.length() - 1;
         boolean isPalindrome = true;
