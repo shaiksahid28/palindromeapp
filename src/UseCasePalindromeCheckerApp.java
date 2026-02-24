aad85f402f46bf2136d1be627858faf9c46e0b29
shaiksahid28 <directorsahidbabu@gmail.com>
2026-02-24 15:50:04 +0530

    UC7

---
+++ src/UseCasePalindromeCheckerApp.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class UseCasePalindromeCheckerApp {
+    public static void main(String[] args) {
+        String input = "level";
+        LinkedList<Character> list = new LinkedList<>();
+
+        for (char c : input.toCharArray()) {
+            list.addLast(c);
+        }
+
+        boolean isPalindrome = true;
+
+        while (list.size() > 1) {
+            if (!list.removeFirst().equals(list.removeLast())) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println("Input : " + input);
+        System.out.println("Is Palindrome? : " + isPalindrome);
+    }
+}
